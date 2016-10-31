016b0c574091ed58d2cf402565cc1eccaf269491
Juan Antonio Sandoval Navarro <Juan Antonio Sandoval Navarro>
2016-10-31 13:13:05 +0100

    version crud 1.0 notas

--- src/NotesBundle/Controller/DefaultController.php
+++ src/NotesBundle/Controller/DefaultController.php
@@ -4,20 +4,54 @@ namespace NotesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use NotesBundle\Entity\Nota;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('NotesBundle:Default:index.html.twig');
+		$notas= $this->getDoctrine()->getRepository("NotesBundle:Nota")->findAll();
+		
+      return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
     }
-
-	public function ayudaAction($tema)
+public function crearAction($titulo,$contenido)
     {
-        //return new Response("<html><body>Esta es la ayuda sobre el tema ".$tema."</body></html>");
+ 		$em = $this->getDoctrine()->getManager();
+		$nota= new Nota();
+		$nota->setTitulo($titulo);
+		$nota->setContenido($contenido);
+		$nota->setFecha(new \DateTime("now"));
+		$em->persist($nota);     
+		$em->flush();
+		$notas= $this->getDoctrine()->getRepository("NotesBundle:Nota")->findAll();
 
-    	return $this->render("NotesBundle:Default:ayuda.html.twig", array("tema"=> $tema));
-
-
+      return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
     }
+
+public function eliminarAction($id)
+    {
+ 		$nota= $this->getDoctrine()->getRepository("NotesBundle:Nota")->findOneById($id);
+ 		$em = $this->getDoctrine()->getManager();
+ 		$em->remove($nota);
+		$em->flush();
+		$notas= $this->getDoctrine()->getRepository("NotesBundle:Nota")->findAll();
+
+      return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
+    }
+
+public function actualizarAction($id,$titulo,$contenido)
+    {
+ 		$em = $this->getDoctrine()->getManager();
+ 		$nota= $this->getDoctrine()->getRepository("NotesBundle:Nota")->findOneById($id);
+		$nota->setTitulo($titulo);
+		$nota->setContenido($contenido);
+		$nota->setFecha(new \DateTime("now"));
+		$em->flush();
+		$notas= $this->getDoctrine()->getRepository("NotesBundle:Nota")->findAll();
+
+      return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
+    }
+
+	
 }
---
+++ src/NotesBundle/Entity/Nota.php~
@@ -0,0 +1,124 @@
+<?php
+
+namespace NotesBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Nota
+ *
+ * @ORM\Table(name="nota")
+ * @ORM\Entity(repositoryClass="NotesBundle\Repository\NotaRepository")
+ */
+class Nota
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="fecha", type="datetime")
+     */
+    private $fecha;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titulo", type="string", length=255)
+     */
+    private $titulo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contenido", type="string", length=255)
+     */
+    private $contenido;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set fecha
+     *
+     * @param \DateTime $fecha
+     * @return Nota
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    /**
+     * Get fecha
+     *
+     * @return \DateTime 
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * Set titulo
+     *
+     * @param string $titulo
+     * @return Nota
+     */
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get titulo
+     *
+     * @return string 
+     */
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set contenido
+     *
+     * @param string $contenido
+     * @return Nota
+     */
+    public function setContenido($contenido)
+    {
+        $this->contenido = $contenido;
+
+        return $this;
+    }
+
+    /**
+     * Get contenido
+     *
+     * @return string 
+     */
+    public function getContenido()
+    {
+        return $this->contenido;
+    }
+}
