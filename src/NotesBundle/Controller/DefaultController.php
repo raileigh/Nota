b02b2007685b50790404e5d50fcab7f953f166c9
Juan Antonio Sandoval Navarro <Juan Antonio Sandoval Navarro>
2016-11-04 11:59:54 +0100

    consulta credenciales del form login a la base de datos y acceso al principal

--- src/NotesBundle/Controller/DefaultController.php
+++ src/NotesBundle/Controller/DefaultController.php
@@ -6,17 +6,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use NotesBundle\Entity\Nota;
+use NotesBundle\Entity\usuario;
+
+
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    
+
+	 public function loginAction()// este metodo no necesito ningun parametro y objeto creado porque lo unico que necesito es mostrar un twig simplemente, no tengo que pasar aun ningun datos.
+    {
+    	
+        return $this->render('NotesBundle:Default:login.html.twig');
+    }
+
+
+	  public function entrarAction(Request $request)
+    {
+
+    	
+		$em = $this->getDoctrine()->getManager(); //creo la variable "$em" y la igualo a la variable $this(controlador) y llamo a su metodo doctrine para que me comunique con la base de datos
+		
+		$nombre = $request->request->get("nombre");// para capturar el valor del formulario por post
+    	$password = $request->request->get("password");// para capturar el valor del formulario por post
+    	$usuario = $this->getDoctrine()->getRepository("NotesBundle:usuario")->findOneBy(array("nombre" => $nombre, "password" => $password));//igualo la variable usuario con el getDoctrine y este al repositorio para que me encuentre en la base de datos el nombre y el pass que el usuario le ha puesto por post desde el formulario y que lo mande a principal
+    	$notas=$this->getDoctrine()->getRepository("NotesBundle:Nota")->findOneBy(array("nombre" => $nombre, "id" => $id));//hago lo mismo con la variable $notas, porque claro yo entro al principal pero si el metodo no tiene esta linea me va a crasear porque nsi no le digo que me muestre las notas me va a dar error al mandarle al principal y no mostrar nada. y le pongo findAll para que me muestre todas las notas.
+
+		return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));		
+	}
+    
+	 public function indexAction()
     {
     	//$request->request->get('request');
 		$notas = $this->getDoctrine()->getRepository("NotesBundle:Nota")->findAll();// me lista las notas que tengo
 		
         return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
     }
-public function crearAction(Request $request)
+	public function crearAction(Request $request)
     {
 		$titulo = $request->request->get('titulo');// para capturar el valor del formulario por post
 		$contenido = $request->request->get('contenido');// para capturar el valor del formulario por post
@@ -32,7 +58,7 @@ public function crearAction(Request $request)
         return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
     }
 
-public function eliminarAction($id)
+	public function eliminarAction($id)
     {
  		$nota = $this->getDoctrine()->getRepository("NotesBundle:Nota")->findOneById($id);
  		$em = $this->getDoctrine()->getManager();
@@ -43,14 +69,14 @@ public function eliminarAction($id)
         return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
     }
 
-public function actualizarAction($id)
-  {
+	public function actualizarAction($id)
+  	{
     	$em = $this->getDoctrine()->getManager();
     	$nota = $this->getDoctrine()->getRepository("NotesBundle:Nota")->findOneById($id);
 		
 		return $this->render('NotesBundle:Default:editar.html.twig', array("nota"=>$nota));
-}
-public function editarAction(Request $request)
+	}
+	public function editarAction(Request $request)
     {
  		$id = $request->request->get('id');// para capturar el valor del formulario por post
  		$titulo = $request->request->get('titulo');// para capturar el valor del formulario por post
@@ -65,6 +91,11 @@ public function editarAction(Request $request)
 
         return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
     }
+
+
+
+
+
 
 	
 }
---
+++ src/NotesBundle/Entity/usuario.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace NotesBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * usuario
+ *
+ * @ORM\Table(name="usuario")
+ * @ORM\Entity(repositoryClass="NotesBundle\Repository\usuarioRepository")
+ */
+class usuario
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255, unique=true)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     * @return usuario
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return usuario
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
