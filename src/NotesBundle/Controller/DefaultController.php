e5b6dfa08eebfe4372c619f9c8c33ffc1632d02a
Juan Antonio Sandoval Navarro <Juan Antonio Sandoval Navarro>
2016-11-05 14:05:21 +0100

    solucionado el problema de ver en la url el nombre de cada metodo, añadiendo redirectroute y creacion de la sesion del usuario

--- src/NotesBundle/Controller/DefaultController.php
+++ src/NotesBundle/Controller/DefaultController.php
@@ -23,35 +23,44 @@ class DefaultController extends Controller
 
 	  public function entrarAction(Request $request)
     {
-
-    	
-		$em = $this->getDoctrine()->getManager(); //creo la variable "$em" y la igualo a la variable $this(controlador) y llamo a su metodo doctrine para que me comunique con la base de datos
 		
 		$nombre = $request->request->get("nombre");// para capturar el valor del formulario por post
     	$password = $request->request->get("password");// para capturar el valor del formulario por post
     	$usuario = $this->getDoctrine()->getRepository("NotesBundle:Usuario")->findOneBy(array("nombre" => $nombre, "password" => $password));//igualo la variable usuario con el getDoctrine y este al repositorio para que me encuentre en la base de datos el nombre y el pass que el usuario le ha puesto por post desde el formulario y que lo mande a principal
     	
-    	$notas = $usuario->getNotas();
-
     	 if (!$usuario)
     	 {
     	  return $this->render('NotesBundle:Default:login.html.twig'); 
     	 } else { 
 
-    	 	 return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
+    	 	$session = $this->getRequest()->getSession();
+    	 	$session->set("id", $usuario->getId());
+
+    	 	 return $this->redirectToRoute('notes_homepage');
     	 }
  	
 	}
     
 	 public function indexAction()
     {
-    	//$request->request->get('request');
-		$notas = $this->getDoctrine()->getRepository("NotesBundle:Nota")->findAll();// me lista las notas que tengo
-		
+    	
+    	$session = $this->getRequest()->getSession();
+		//$session->get("id");(al crear el objeto en el metodo de arriba, $session = $this->getRequest()->getSession(); ya tiene sus metodos los set y los get,  creo  $session->get("id") para que me recoja el id del usuario que ya habiamos puesto en el parametro $session->set("id", $usuario->getId()); )
+		$usuario = $this->getDoctrine()->getRepository("NotesBundle:Usuario")->findOneById($session->get("id"));
+
+		 if (!$usuario)
+    	 {
+    	  return $this->render('NotesBundle:Default:login.html.twig'); 
+    	 }
+
+		$notas = $usuario->getNotas();
+
         return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
     }
 	public function crearAction(Request $request)
     {
+    	$session = $this->getRequest()->getSession();
+    	$usuario = $this->getDoctrine()->getRepository("NotesBundle:Usuario")->findOneById($session->get("id"));
 		$titulo = $request->request->get('titulo');// para capturar el valor del formulario por post
 		$contenido = $request->request->get('contenido');// para capturar el valor del formulario por post
 		$em = $this->getDoctrine()->getManager(); //creo la variable "$em" y la igualo a la variable $this(controlador) y llamo a su metodo doctrine para que me comunique con la base de datos
@@ -59,11 +68,12 @@ class DefaultController extends Controller
 		$nota->setTitulo($titulo);// a esa variable meto la info que quiero darle, en este caso titulo
 		$nota->setContenido($contenido);// al igual que anteriormente en este caso contenido
 		$nota->setFecha(new \DateTime("now"));// y por último meterle una fecha actual al crear esa nota que queremos
+		$nota->setUsuario($usuario);//le doy todo contenido del objeto usuario (en este caso) y relacionarla con ella
 		$em->persist($nota); // con el metodo persist puedo pasarle la variable en cuestion en este caso nota y que la meta en la base de datos    
 		$em->flush();// y con el metodo flush lo guardo en la base de datos
-		$notas=$this->getDoctrine()->getRepository("NotesBundle:Nota")->findAll();// me lista las notas que vuelvo a tener
+		
 
-        return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
+        return $this->redirectToRoute('notes_homepage');
     }
 
 	public function eliminarAction($id)
@@ -72,9 +82,8 @@ class DefaultController extends Controller
  		$em = $this->getDoctrine()->getManager();
  		$em->remove($nota);
 		$em->flush();
-		$notas=$this->getDoctrine()->getRepository("NotesBundle:Nota")->findAll();
-
-        return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
+		
+		return $this->redirectToRoute('notes_homepage');
     }
 
 	public function actualizarAction($id)
@@ -95,9 +104,8 @@ class DefaultController extends Controller
 		$nota->setContenido($contenido);
 		$nota->setFecha(new \DateTime("now"));
 		$em->flush();
-		$notas=$this->getDoctrine()->getRepository("NotesBundle:Nota")->findAll();
-
-        return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
+		
+        return $this->redirectToRoute('notes_homepage');
     }
 
 
