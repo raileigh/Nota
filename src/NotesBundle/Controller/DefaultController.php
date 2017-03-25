cd31aca903f89020cab8e16f0cc6f738fe8321bb
Juan Antonio Sandoval Navarro <Juan Antonio Sandoval Navarro>
2017-03-25 12:35:00 +0100

    Modificacion de vistas, routing y controller

--- src/NotesBundle/Controller/DefaultController.php
+++ src/NotesBundle/Controller/DefaultController.php
@@ -12,13 +12,12 @@ use NotesBundle\Entity\Usuario;
 
 class DefaultController extends Controller
 {
-    
 
-	 public function loginAction()// este metodo no necesito ningun parametro ni objeto creado porque lo unico que necesito es mostrar un twig simplemente, no tengo que pasar aun ningun dato.
-    {
-    	$usuario = new Usuario();// Creo la variable obejo ya que luego tengo q la necesito para poder rescatar tu id
+
+    public function loginAction(){
+    	$usuario = new Usuario();// Creo la variable usuario ya que luego la necesito para poder rescatar su id
     	$session = $this->getRequest()->getSession();// creo la variable sesión y le digo que me captura la sesion que esta abierta
-    	$id = $session->get("id", $usuario->getId());// creo la variable id ( le pongo el nombre id para que vaya acorde con lo que deseo pero podria ser cualquier otro nombre) y la igualo al objeto sesion que al crearla anteriormente tiene sus propias funciones en este caso necesito rescatar el id por eso utilizo get que esta compuesto por ("",$) en este caso pongo "id", $usuario->getId() esta ultima variable quiere decir que me coja el id del objeto usuario que esta en la sesión y que lo guarde en la variable id qye posteriormente voy a utilizarla para el if.
+    	$id = $session->get("id", $usuario->getId());// creo la variable id ( le pongo el nombre id para que vaya acorde con lo que deseo pero podria ser cualquier otro nombre) y la igualo al objeto sesion que al crearla anteriormente tiene sus propias funciones en este caso necesito rescatar el id por eso utilizo get que esta compuesto por ("",$) en este caso pongo "id", $usuario->getId() esta ultima variable quiere decir que me coja el id del objeto usuario que esta en la sesión y que lo guarde en la variable id que posteriormente voy a utilizarla para el if.(esta linea es erronea, sería para un set, un get solo quiere la key y te devuelve el valor asociado)
     	
     	
     	
@@ -27,51 +26,49 @@ class DefaultController extends Controller
     		return $this->render('NotesBundle:Default:login.html.twig');
 
     	}else{//si no mandame a principal, en el caso de que este logueado y este dentro ya y quiera cambiar la url a login, al estar logueado permanecera en principal
-    		 return $this->redirectToRoute('notes_homepage');
-    	}
-       
+         return $this->redirectToRoute('notes_homepage');
+        }
+
     }
 
 
-	  public function entrarAction(Request $request)
-    {
-		
+    public function entrarAction(Request $request){
+
 		$nombre = $request->request->get("nombre");// para capturar el valor del formulario por post
     	$password = $request->request->get("password");// para capturar el valor del formulario por post
     	$usuario = $this->getDoctrine()->getRepository("NotesBundle:Usuario")->findOneByNombre($nombre);//igualo la variable usuario con el getDoctrine y este al repositorio para que me encuentre en la base de datos el nombre que el usuario le ha puesto por post desde el formulario y que lo mande a principal
-        
+
         if (!$usuario){
 
-           return $this->render('NotesBundle:Default:registrar.html.twig'); 
+         return $this->render('NotesBundle:Default:registrar.html.twig'); 
 
-    	 } 
+        } 
 
-         $pass = $usuario->getPassword();
-         if(password_verify($password, $pass)==false){
+     $pass = $usuario->getPassword();
+     if(password_verify($password, $pass)==false){
 
-            return $this->render('NotesBundle:Default:registrar.html.twig'); 
-           
-    	 } elseif($usuario || password_verify($password, $pass)){
+        return $this->render('NotesBundle:Default:registrar.html.twig'); 
+
+        }elseif($usuario || password_verify($password, $pass)){
 
              $session = $this->getRequest()->getSession(); //establece la sesión
              $session->set("id", $usuario->getId());// y meteme el id del id del usuario en esa sesión y mandame a principal
 
              return $this->redirectToRoute('notes_homepage');
 
-         }
-	}
+        }
+    }
 
 
-	 public function cerrarAction()
-    {
+    public function cerrarAction(){
     	$session = $this->getRequest()->getSession();//este metodo es para crear una sesion y poder llamar a sus funciones
     	$session->clear();//una de las funciones que tienen es clear(); para poder limpiar todos los atributos de la sesión y así poder cerrarla
         return $this->redirectToRoute('notes_login');//retornamos al login
     }
 
-	  public function registrarAction(Request $request)// al metodo lo mandamos por post para q n vea información el usuario
+	public function registrarAction(Request $request)// al metodo lo mandamos por post para q n vea información el usuario
     {
-		
+
 		$nombre = $request->request->get("nombre");// para capturar el valor del formulario por post
     	$password = $request->request->get("password");// para capturar el valor del formulario por post
     	$usuario = $this->getDoctrine()->getRepository("NotesBundle:Usuario")->findOneByNombre($nombre);//igualo la variable usuario con el getDoctrine y este al repositorio para que me encuentre en la base de datos el nombre que el usuario le ha puesto por post desde el formulario y que lo mande a principal
@@ -90,34 +87,32 @@ class DefaultController extends Controller
 
 
     	 	 return $this->redirectToRoute('notes_login');//llevame al login
-    	 
-    	 } else { 
-    	
+
+          } else { 
+
     	 	return $this->redirectToRoute('notes_login');//al estar ya registrado lo mandamos otra vez al login.
     	 }
- 	
-	}
 
-	 public function indexAction()
-    {
-    	
-    	$session = $this->getRequest()->getSession();
-		//$session->get("id");(al crear el objeto en el metodo de arriba, $session = $this->getRequest()->getSession(); ya tiene sus metodos los set y los get,  creo  $session->get("id") para que me recoja el id del usuario que ya habiamos puesto en el parametro $session->set("id", $usuario->getId()); )
-		$usuario = $this->getDoctrine()->getRepository("NotesBundle:Usuario")->findOneById($session->get("id"));
-
-		 if (!$usuario)
-    	 {
-    	  return $this->render('NotesBundle:Default:login.html.twig'); 
-    	 }
-
-		$notas = $usuario->getNotas();
-
-        return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
     }
-	public function crearAction(Request $request)
-    {
-    	$session = $this->getRequest()->getSession();
-    	$usuario = $this->getDoctrine()->getRepository("NotesBundle:Usuario")->findOneById($session->get("id"));
+
+    public function indexAction(){
+
+           $session = $this->getRequest()->getSession();
+		//$session->get("id");(al crear el objeto en el metodo de arriba, $session = $this->getRequest()->getSession(); ya tiene sus metodos los set y los get,  creo  $session->get("id") para que me recoja el id del usuario que ya habiamos puesto en el parametro $session->set("id", $usuario->getId()); )
+           $usuario = $this->getDoctrine()->getRepository("NotesBundle:Usuario")->findOneById($session->get("id"));
+
+           if (!$usuario)
+           {
+             return $this->render('NotesBundle:Default:login.html.twig'); 
+         }
+
+         $notas = $usuario->getNotas();
+
+         return $this->render('NotesBundle:Default:principal.html.twig', array("notas"=>$notas));
+     }
+    public function crearAction(Request $request){
+       $session = $this->getRequest()->getSession();
+       $usuario = $this->getDoctrine()->getRepository("NotesBundle:Usuario")->findOneById($session->get("id"));
 		$titulo = $request->request->get('titulo');// para capturar el valor del formulario por post
 		$contenido = $request->request->get('contenido');// para capturar el valor del formulario por post
 		$em = $this->getDoctrine()->getManager(); //creo la variable "$em" y la igualo a la variable $this(controlador) y llamo a su metodo doctrine para que me comunique con la base de datos
@@ -133,36 +128,34 @@ class DefaultController extends Controller
         return $this->redirectToRoute('notes_homepage');
     }
 
-	public function eliminarAction($id)
-    {
- 		$nota = $this->getDoctrine()->getRepository("NotesBundle:Nota")->findOneById($id);
- 		$em = $this->getDoctrine()->getManager();
- 		$em->remove($nota);
-		$em->flush();
-		
-		return $this->redirectToRoute('notes_homepage');
+    public function eliminarAction($id){
+     $nota = $this->getDoctrine()->getRepository("NotesBundle:Nota")->findOneById($id);
+     $em = $this->getDoctrine()->getManager();
+     $em->remove($nota);
+     $em->flush();
+
+     return $this->redirectToRoute('notes_homepage');
     }
 
-	public function actualizarAction($id)
-  	{
-    	$em = $this->getDoctrine()->getManager();
-    	$nota = $this->getDoctrine()->getRepository("NotesBundle:Nota")->findOneById($id);
-		
-		return $this->render('NotesBundle:Default:editar.html.twig', array("nota"=>$nota));
-	}
-	public function editarAction(Request $request)
-    {
+    public function actualizarAction($id){
+   $em = $this->getDoctrine()->getManager();
+   $nota = $this->getDoctrine()->getRepository("NotesBundle:Nota")->findOneById($id);
+
+   return $this->render('NotesBundle:Default:editar.html.twig', array("nota"=>$nota));
+    }
+
+    public function editarAction(Request $request){
  		$id = $request->request->get('id');// para capturar el valor del formulario por post
  		$titulo = $request->request->get('titulo');// para capturar el valor del formulario por post
 		$contenido = $request->request->get('contenido');// para capturar el valor del formulario por post
- 		$em = $this->getDoctrine()->getManager();
- 		$nota = $this->getDoctrine()->getRepository("NotesBundle:Nota")->findOneById($id);
-		$nota->setTitulo($titulo);
-		$nota->setContenido($contenido);
-		$nota->setFecha(new \DateTime("now"));
-		$em->flush();
-		
-        return $this->redirectToRoute('notes_homepage');
+     $em = $this->getDoctrine()->getManager();
+     $nota = $this->getDoctrine()->getRepository("NotesBundle:Nota")->findOneById($id);
+     $nota->setTitulo($titulo);
+     $nota->setContenido($contenido);
+     $nota->setFecha(new \DateTime("now"));
+     $em->flush();
+
+     return $this->redirectToRoute('notes_homepage');
     }
 
 
@@ -170,5 +163,5 @@ class DefaultController extends Controller
 
 
 
-	
+
 }
